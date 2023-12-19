f59d95e4c91d4167268b597ee5028855d397b723
Nicholas Roether <nicholas.roether@t-online.de>
2023-12-20 00:22:51 +0100

    Implement writing metadata and a bunch of tests

--- acorn/src/storage/mod.rs
+++ acorn/src/storage/mod.rs
@@ -1,4 +1,4 @@
-use std::io;
+use std::{io, usize};
 
 use thiserror::Error;
 
@@ -9,7 +9,7 @@ mod file;
 #[derive(Debug, Error)]
 pub enum Error {
     #[error("The provided file is not an acorn storage file (expected magic bytes {MAGIC:08x?})")]
-    Magic,
+    NotAStorageFile,
 
     #[error("The format version {0} is not supported in this version of acorn")]
     UnsupportedVersion(u8),
@@ -17,10 +17,14 @@ pub enum Error {
     #[error("The storage is corrupted")]
     CorruptedMeta,
 
+    #[error("Failed to expand storage file")]
+    IncompleteWrite,
+
     #[error(transparent)]
     Io(#[from] io::Error),
 }
 
+const CURRENT_VERSION: u8 = 1;
 const MAGIC: [u8; 4] = *b"ACRN";
 
 /*
@@ -35,19 +39,27 @@ const MAGIC: [u8; 4] = *b"ACRN";
  *
  */
 
+#[derive(Debug, Clone)]
 struct Meta {
-    format_version: u8,
-    page_size: usize,
+    pub format_version: u8,
+    pub page_size: usize,
 }
 
 impl Meta {
-    fn read_from(file: impl StorageFile) -> Result<Self, Error> {
+    fn new(page_size: usize) -> Self {
+        Self {
+            format_version: CURRENT_VERSION,
+            page_size,
+        }
+    }
+
+    fn read_from(file: &impl StorageFile) -> Result<Self, Error> {
         let mut buffer: [u8; 32] = Default::default();
         let bytes_read = file.read_at(&mut buffer, 0)?;
 
         let magic: [u8; 4] = buffer[0..4].try_into().unwrap();
         if magic != MAGIC {
-            return Err(Error::Magic);
+            return Err(Error::NotAStorageFile);
         }
 
         if bytes_read != buffer.len() {
@@ -65,5 +77,74 @@ impl Meta {
             format_version,
             page_size,
         })
+    }
+
+    fn write_to(&self, file: &mut impl StorageFile) -> Result<(), Error> {
+        let mut buf: [u8; 32] = Default::default();
+
+        buf[0..4].copy_from_slice(&MAGIC);
+        buf[5] = self.format_version;
+        buf[5..9].copy_from_slice(&self.page_size.to_be_bytes());
+
+        if file.write_at(&buf, 0)? != buf.len() {
+            return Err(Error::IncompleteWrite);
+        }
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::assert_matches::assert_matches;
+
+    use super::*;
+
+    #[test]
+    fn read_metadata() {
+        let data = vec![
+            0x41, 0x43, 0x52, 0x4e, 0x01, 0x00, 0x00, 0x04, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        ];
+
+        let meta = Meta::read_from(&data).unwrap();
+        assert_eq!(meta.format_version, 1);
+        assert_eq!(meta.page_size, 1024);
+    }
+
+    #[test]
+    fn try_read_with_invalid_magic() {
+        let data = vec![
+            0x42, 0x43, 0x52, 0x4e, 0x01, 0x00, 0x00, 0x04, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        ];
+
+        assert_matches!(Meta::read_from(&data), Err(Error::NotAStorageFile));
+    }
+
+    #[test]
+    fn try_read_with_unsupported_version() {
+        let data = vec![
+            0x41, 0x43, 0x52, 0x4e, 0x02, 0x00, 0x00, 0x04, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        ];
+
+        assert_matches!(Meta::read_from(&data), Err(Error::UnsupportedVersion(..)));
+    }
+
+    #[test]
+    fn try_read_incomplete_magic() {
+        let data = vec![0x41, 0x43];
+
+        assert_matches!(Meta::read_from(&data), Err(Error::NotAStorageFile));
+    }
+
+    #[test]
+    fn try_read_incomplete_meta() {
+        let data = vec![0x41, 0x43, 0x52, 0x4e, 0x01, 0x00, 0x00];
+
+        assert_matches!(Meta::read_from(&data), Err(Error::CorruptedMeta));
     }
 }
