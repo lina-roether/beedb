d290407cec415754335ffeb64952901d6fc06e74
Nicholas Roether <nicholas.roether@t-online.de>
2023-12-21 11:25:11 +0100

    Customize rustfmt

--- acorn/src/storage/file.rs
+++ acorn/src/storage/file.rs
@@ -7,63 +7,63 @@ use std::os::unix::fs::FileExt;
 use std::os::windows::fs::FileExt;
 
 pub trait StorageFile {
-    fn read_at(&self, buf: &mut [u8], offset: u64) -> io::Result<usize>;
+	fn read_at(&self, buf: &mut [u8], offset: u64) -> io::Result<usize>;
 
-    fn write_at(&mut self, buf: &[u8], offset: u64) -> io::Result<usize>;
+	fn write_at(&mut self, buf: &[u8], offset: u64) -> io::Result<usize>;
 }
 
 fn get_buf_range(len: usize, buf_len: usize, offset: u64) -> Range<usize> {
-    if offset >= len as u64 {
-        return 0..0;
-    };
-    let start = offset as usize;
-    start..usize::min(start + buf_len, len)
+	if offset >= len as u64 {
+		return 0..0;
+	};
+	let start = offset as usize;
+	start..usize::min(start + buf_len, len)
 }
 
 impl StorageFile for Vec<u8> {
-    fn read_at(&self, buf: &mut [u8], offset: u64) -> io::Result<usize> {
-        let range = get_buf_range(self.len(), buf.len(), offset);
-        let num_read = range.len();
-        buf[0..num_read].copy_from_slice(&self[range]);
-        Ok(num_read)
-    }
+	fn read_at(&self, buf: &mut [u8], offset: u64) -> io::Result<usize> {
+		let range = get_buf_range(self.len(), buf.len(), offset);
+		let num_read = range.len();
+		buf[0..num_read].copy_from_slice(&self[range]);
+		Ok(num_read)
+	}
 
-    fn write_at(&mut self, buf: &[u8], offset: u64) -> io::Result<usize> {
-        let min_length = (offset as usize) + buf.len();
-        if self.len() < min_length {
-            self.resize(min_length, 0);
-        }
-        let range = get_buf_range(self.len(), buf.len(), offset);
-        let num_written = range.len();
-        self[range].copy_from_slice(&buf[0..num_written]);
-        Ok(num_written)
-    }
+	fn write_at(&mut self, buf: &[u8], offset: u64) -> io::Result<usize> {
+		let min_length = (offset as usize) + buf.len();
+		if self.len() < min_length {
+			self.resize(min_length, 0);
+		}
+		let range = get_buf_range(self.len(), buf.len(), offset);
+		let num_written = range.len();
+		self[range].copy_from_slice(&buf[0..num_written]);
+		Ok(num_written)
+	}
 }
 
 cfg_match! {
-    cfg(unix) => {
-        impl StorageFile for File {
-            fn read_at(&self, buf: &mut [u8], offset: u64) -> io::Result<usize> {
-                FileExt::read_at(self, buf, offset)
-            }
+	cfg(unix) => {
+		impl StorageFile for File {
+			fn read_at(&self, buf: &mut [u8], offset: u64) -> io::Result<usize> {
+				FileExt::read_at(self, buf, offset)
+			}
 
-            fn write_at(&mut self, buf: &[u8], offset: u64) -> io::Result<usize> {
-                FileExt::write_at(self, buf, offset)
-            }
-        }
-    }
-    cfg(windows) => {
-        impl StorageFile for File {
-            fn read_at(&self, buf: &mut [u8], offset: u64) -> io::Result<usize> {
-                FileExt::seek_read(self, buf, offset)
-            }
+			fn write_at(&mut self, buf: &[u8], offset: u64) -> io::Result<usize> {
+				FileExt::write_at(self, buf, offset)
+			}
+		}
+	}
+	cfg(windows) => {
+		impl StorageFile for File {
+			fn read_at(&self, buf: &mut [u8], offset: u64) -> io::Result<usize> {
+				FileExt::seek_read(self, buf, offset)
+			}
 
-            fn write_at(&mut self, buf: &[u8], offset: u64) -> io::Result<usize> {
-                FileExt::seek_write(self, buf, offset)
-            }
-        }
-    }
-    _ => {
-        compile_error!("The Acorn storage engine is not supported on this platform");
-    }
+			fn write_at(&mut self, buf: &[u8], offset: u64) -> io::Result<usize> {
+				FileExt::seek_write(self, buf, offset)
+			}
+		}
+	}
+	_ => {
+		compile_error!("The Acorn storage engine is not supported on this platform");
+	}
 }
--- acorn/src/storage/format.rs
+++ acorn/src/storage/format.rs
@@ -6,20 +6,20 @@ use super::file::StorageFile;
 
 #[derive(Debug, Error)]
 pub enum Error {
-    #[error("The provided file is not an acorn storage file (expected magic bytes {MAGIC:08x?})")]
-    NotAStorageFile,
+	#[error("The provided file is not an acorn storage file (expected magic bytes {MAGIC:08x?})")]
+	NotAStorageFile,
 
-    #[error("The format version {0} is not supported in this version of acorn")]
-    UnsupportedVersion(u8),
+	#[error("The format version {0} is not supported in this version of acorn")]
+	UnsupportedVersion(u8),
 
-    #[error("The storage is corrupted (Unexpected end of file)")]
-    UnexpectedEOF,
+	#[error("The storage is corrupted (Unexpected end of file)")]
+	UnexpectedEOF,
 
-    #[error("Failed to expand storage file")]
-    IncompleteWrite,
+	#[error("Failed to expand storage file")]
+	IncompleteWrite,
 
-    #[error(transparent)]
-    Io(#[from] io::Error),
+	#[error(transparent)]
+	Io(#[from] io::Error),
 }
 
 const CURRENT_VERSION: u8 = 1;
@@ -39,53 +39,53 @@ const MAGIC: [u8; 4] = *b"ACRN";
 
 #[derive(Debug, Clone)]
 pub struct Meta {
-    pub format_version: u8,
-    pub page_size_exponent: u8,
-    pub page_size: usize,
+	pub format_version: u8,
+	pub page_size_exponent: u8,
+	pub page_size: usize,
 }
 
 impl Meta {
-    const SIZE: usize = 32;
+	const SIZE: usize = 32;
 
-    pub fn read_from(file: &impl StorageFile) -> Result<Self, Error> {
-        let mut buffer: [u8; Self::SIZE] = Default::default();
-        let bytes_read = file.read_at(&mut buffer, 0)?;
+	pub fn read_from(file: &impl StorageFile) -> Result<Self, Error> {
+		let mut buffer: [u8; Self::SIZE] = Default::default();
+		let bytes_read = file.read_at(&mut buffer, 0)?;
 
-        let magic: [u8; 4] = buffer[0..4].try_into().unwrap();
-        if magic != MAGIC {
-            return Err(Error::NotAStorageFile);
-        }
+		let magic: [u8; 4] = buffer[0..4].try_into().unwrap();
+		if magic != MAGIC {
+			return Err(Error::NotAStorageFile);
+		}
 
-        if bytes_read != buffer.len() {
-            return Err(Error::UnexpectedEOF);
-        }
+		if bytes_read != buffer.len() {
+			return Err(Error::UnexpectedEOF);
+		}
 
-        let format_version = buffer[4];
-        if format_version != 1 {
-            return Err(Error::UnsupportedVersion(format_version));
-        }
+		let format_version = buffer[4];
+		if format_version != 1 {
+			return Err(Error::UnsupportedVersion(format_version));
+		}
 
-        let page_size_exponent = u8::from_be_bytes(buffer[5..6].try_into().unwrap());
+		let page_size_exponent = u8::from_be_bytes(buffer[5..6].try_into().unwrap());
 
-        Ok(Self {
-            format_version,
-            page_size_exponent,
-            page_size: 1 << page_size_exponent,
-        })
-    }
+		Ok(Self {
+			format_version,
+			page_size_exponent,
+			page_size: 1 << page_size_exponent,
+		})
+	}
 
-    pub fn write_to(&self, file: &mut impl StorageFile) -> Result<(), Error> {
-        let mut buf: [u8; Self::SIZE] = Default::default();
+	pub fn write_to(&self, file: &mut impl StorageFile) -> Result<(), Error> {
+		let mut buf: [u8; Self::SIZE] = Default::default();
 
-        buf[0..4].copy_from_slice(&MAGIC);
-        buf[4] = self.format_version;
-        buf[5..6].copy_from_slice(&self.page_size_exponent.to_be_bytes());
+		buf[0..4].copy_from_slice(&MAGIC);
+		buf[4] = self.format_version;
+		buf[5..6].copy_from_slice(&self.page_size_exponent.to_be_bytes());
 
-        if file.write_at(&buf, 0)? != buf.len() {
-            return Err(Error::IncompleteWrite);
-        }
-        Ok(())
-    }
+		if file.write_at(&buf, 0)? != buf.len() {
+			return Err(Error::IncompleteWrite);
+		}
+		Ok(())
+	}
 }
 
 /*
@@ -102,246 +102,246 @@ impl Meta {
 
 #[derive(Debug)]
 pub struct State {
-    pub num_pages: u32,
-    pub freelist_trunk: u32,
-    pub freelist_length: u32,
+	pub num_pages: u32,
+	pub freelist_trunk: u32,
+	pub freelist_length: u32,
 }
 
 impl State {
-    const SIZE: usize = 32;
-    const OFFSET: u64 = Meta::SIZE as u64;
+	const SIZE: usize = 32;
+	const OFFSET: u64 = Meta::SIZE as u64;
 
-    pub fn read_from(file: &impl StorageFile) -> Result<Self, Error> {
-        let mut buf: [u8; Self::SIZE] = Default::default();
-        if file.read_at(&mut buf, Self::OFFSET)? != buf.len() {
-            return Err(Error::UnexpectedEOF);
-        }
+	pub fn read_from(file: &impl StorageFile) -> Result<Self, Error> {
+		let mut buf: [u8; Self::SIZE] = Default::default();
+		if file.read_at(&mut buf, Self::OFFSET)? != buf.len() {
+			return Err(Error::UnexpectedEOF);
+		}
 
-        let num_pages = u32::from_be_bytes(buf[0..4].try_into().unwrap());
-        let freelist_trunk = u32::from_be_bytes(buf[4..8].try_into().unwrap());
-        let freelist_length = u32::from_be_bytes(buf[8..12].try_into().unwrap());
+		let num_pages = u32::from_be_bytes(buf[0..4].try_into().unwrap());
+		let freelist_trunk = u32::from_be_bytes(buf[4..8].try_into().unwrap());
+		let freelist_length = u32::from_be_bytes(buf[8..12].try_into().unwrap());
 
-        Ok(Self {
-            num_pages,
-            freelist_trunk,
-            freelist_length,
-        })
-    }
+		Ok(Self {
+			num_pages,
+			freelist_trunk,
+			freelist_length,
+		})
+	}
 
-    pub fn write_to(&self, file: &mut impl StorageFile) -> Result<(), Error> {
-        let mut buf: [u8; Self::SIZE] = Default::default();
+	pub fn write_to(&self, file: &mut impl StorageFile) -> Result<(), Error> {
+		let mut buf: [u8; Self::SIZE] = Default::default();
 
-        buf[0..4].copy_from_slice(&self.num_pages.to_be_bytes());
-        buf[4..8].copy_from_slice(&self.freelist_trunk.to_be_bytes());
-        buf[8..12].copy_from_slice(&self.freelist_length.to_be_bytes());
+		buf[0..4].copy_from_slice(&self.num_pages.to_be_bytes());
+		buf[4..8].copy_from_slice(&self.freelist_trunk.to_be_bytes());
+		buf[8..12].copy_from_slice(&self.freelist_length.to_be_bytes());
 
-        if file.write_at(&buf, Self::OFFSET)? != buf.len() {
-            return Err(Error::IncompleteWrite);
-        }
+		if file.write_at(&buf, Self::OFFSET)? != buf.len() {
+			return Err(Error::IncompleteWrite);
+		}
 
-        Ok(())
-    }
+		Ok(())
+	}
 }
 
 const PAGE_SECTION_OFFSET: u64 = Meta::SIZE as u64 + State::SIZE as u64;
 
 pub struct PageStorage<F: StorageFile> {
-    page_size: usize,
-    file: F,
+	page_size: usize,
+	file: F,
 }
 
 impl<F: StorageFile> PageStorage<F> {
-    pub fn new(file: F, page_size: usize) -> Self {
-        Self { file, page_size }
-    }
+	pub fn new(file: F, page_size: usize) -> Self {
+		Self { file, page_size }
+	}
 
-    pub fn read_page(&self, buf: &mut [u8], page_number: u32) -> Result<(), Error> {
-        debug_assert_eq!(buf.len(), self.page_size);
+	pub fn read_page(&self, buf: &mut [u8], page_number: u32) -> Result<(), Error> {
+		debug_assert_eq!(buf.len(), self.page_size);
 
-        let offset = self.page_offset(page_number);
-        let bytes_read = self.file.read_at(&mut buf[0..self.page_size], offset)?;
-        if bytes_read != self.page_size {
-            return Err(Error::UnexpectedEOF);
-        }
+		let offset = self.page_offset(page_number);
+		let bytes_read = self.file.read_at(&mut buf[0..self.page_size], offset)?;
+		if bytes_read != self.page_size {
+			return Err(Error::UnexpectedEOF);
+		}
 
-        Ok(())
-    }
+		Ok(())
+	}
 
-    pub fn write_page(&mut self, buf: &[u8], page_number: u32) -> Result<(), Error> {
-        debug_assert_eq!(buf.len(), self.page_size);
+	pub fn write_page(&mut self, buf: &[u8], page_number: u32) -> Result<(), Error> {
+		debug_assert_eq!(buf.len(), self.page_size);
 
-        let offset = self.page_offset(page_number);
-        let bytes_written = self.file.write_at(&buf[0..self.page_size], offset)?;
-        if bytes_written != self.page_size {
-            return Err(Error::IncompleteWrite);
-        }
+		let offset = self.page_offset(page_number);
+		let bytes_written = self.file.write_at(&buf[0..self.page_size], offset)?;
+		if bytes_written != self.page_size {
+			return Err(Error::IncompleteWrite);
+		}
 
-        Ok(())
-    }
+		Ok(())
+	}
 
-    fn page_offset(&self, page_number: u32) -> u64 {
-        PAGE_SECTION_OFFSET + (page_number as u64) * (self.page_size as u64)
-    }
+	fn page_offset(&self, page_number: u32) -> u64 {
+		PAGE_SECTION_OFFSET + (page_number as u64) * (self.page_size as u64)
+	}
 }
 
 #[cfg(test)]
 mod tests {
-    use std::assert_matches::assert_matches;
+	use std::assert_matches::assert_matches;
 
-    use super::*;
+	use super::*;
 
-    #[test]
-    fn read_metadata() {
-        let data = vec![
-            0x41, 0x43, 0x52, 0x4e, 0x01, 0x0a, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-        ];
+	#[test]
+	fn read_metadata() {
+		let data = vec![
+			0x41, 0x43, 0x52, 0x4e, 0x01, 0x0a, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+			0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+			0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+		];
 
-        let meta = Meta::read_from(&data).unwrap();
-        assert_eq!(meta.format_version, 1);
-        assert_eq!(meta.page_size, 1024);
-    }
+		let meta = Meta::read_from(&data).unwrap();
+		assert_eq!(meta.format_version, 1);
+		assert_eq!(meta.page_size, 1024);
+	}
 
-    #[test]
-    fn try_read_with_invalid_magic() {
-        let data = vec![
-            0x42, 0x43, 0x52, 0x4e, 0x01, 0x00, 0x00, 0x04, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-        ];
+	#[test]
+	fn try_read_with_invalid_magic() {
+		let data = vec![
+			0x42, 0x43, 0x52, 0x4e, 0x01, 0x00, 0x00, 0x04, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+			0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+			0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+		];
 
-        assert_matches!(Meta::read_from(&data), Err(Error::NotAStorageFile));
-    }
+		assert_matches!(Meta::read_from(&data), Err(Error::NotAStorageFile));
+	}
 
-    #[test]
-    fn try_read_with_unsupported_version() {
-        let data = vec![
-            0x41, 0x43, 0x52, 0x4e, 0x02, 0x00, 0x00, 0x04, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-        ];
+	#[test]
+	fn try_read_with_unsupported_version() {
+		let data = vec![
+			0x41, 0x43, 0x52, 0x4e, 0x02, 0x00, 0x00, 0x04, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+			0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+			0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+		];
 
-        assert_matches!(Meta::read_from(&data), Err(Error::UnsupportedVersion(..)));
-    }
+		assert_matches!(Meta::read_from(&data), Err(Error::UnsupportedVersion(..)));
+	}
 
-    #[test]
-    fn try_read_incomplete_magic() {
-        let data = vec![0x41, 0x43];
+	#[test]
+	fn try_read_incomplete_magic() {
+		let data = vec![0x41, 0x43];
 
-        assert_matches!(Meta::read_from(&data), Err(Error::NotAStorageFile));
-    }
+		assert_matches!(Meta::read_from(&data), Err(Error::NotAStorageFile));
+	}
 
-    #[test]
-    fn try_read_incomplete_meta() {
-        let data = vec![0x41, 0x43, 0x52, 0x4e, 0x01, 0x00, 0x00];
+	#[test]
+	fn try_read_incomplete_meta() {
+		let data = vec![0x41, 0x43, 0x52, 0x4e, 0x01, 0x00, 0x00];
 
-        assert_matches!(Meta::read_from(&data), Err(Error::UnexpectedEOF));
-    }
+		assert_matches!(Meta::read_from(&data), Err(Error::UnexpectedEOF));
+	}
 
-    #[test]
-    fn write_metadata() {
-        let mut data = Vec::new();
+	#[test]
+	fn write_metadata() {
+		let mut data = Vec::new();
 
-        let meta = Meta {
-            format_version: 1,
-            page_size_exponent: 10,
-            page_size: 1024,
-        };
+		let meta = Meta {
+			format_version: 1,
+			page_size_exponent: 10,
+			page_size: 1024,
+		};
 
-        meta.write_to(&mut data).unwrap();
+		meta.write_to(&mut data).unwrap();
 
-        assert_eq!(
-            data,
-            vec![
-                0x41, 0x43, 0x52, 0x4e, 0x01, 0x0a, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-                0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-                0x00, 0x00, 0x00, 0x00
-            ]
-        )
-    }
+		assert_eq!(
+			data,
+			vec![
+				0x41, 0x43, 0x52, 0x4e, 0x01, 0x0a, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+				0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+				0x00, 0x00, 0x00, 0x00
+			]
+		)
+	}
 
-    #[test]
-    fn read_state() {
-        let data = vec![
-            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x45, 0x00, 0x00, 0x01, 0xa4, 0x00, 0x00,
-            0xa4, 0x55, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-        ];
+	#[test]
+	fn read_state() {
+		let data = vec![
+			0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+			0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+			0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x45, 0x00, 0x00, 0x01, 0xa4, 0x00, 0x00,
+			0xa4, 0x55, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+			0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+		];
 
-        let state = State::read_from(&data).unwrap();
+		let state = State::read_from(&data).unwrap();
 
-        assert_eq!(state.num_pages, 69);
-        assert_eq!(state.freelist_trunk, 420);
-        assert_eq!(state.freelist_length, 42069);
-    }
+		assert_eq!(state.num_pages, 69);
+		assert_eq!(state.freelist_trunk, 420);
+		assert_eq!(state.freelist_length, 42069);
+	}
 
-    #[test]
-    fn try_read_incomplete_state() {
-        let data = vec![0x00, 0x00, 0x00];
+	#[test]
+	fn try_read_incomplete_state() {
+		let data = vec![0x00, 0x00, 0x00];
 
-        assert_matches!(State::read_from(&data), Err(Error::UnexpectedEOF));
-    }
+		assert_matches!(State::read_from(&data), Err(Error::UnexpectedEOF));
+	}
 
-    #[test]
-    fn write_state() {
-        let mut data = Vec::<u8>::new();
+	#[test]
+	fn write_state() {
+		let mut data = Vec::<u8>::new();
 
-        let state = State {
-            num_pages: 123,
-            freelist_trunk: 543,
-            freelist_length: 5432,
-        };
-        state.write_to(&mut data).unwrap();
+		let state = State {
+			num_pages: 123,
+			freelist_trunk: 543,
+			freelist_length: 5432,
+		};
+		state.write_to(&mut data).unwrap();
 
-        assert_eq!(
-            data,
-            vec![
-                0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-                0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-                0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x7b, 0x00, 0x00, 0x02, 0x1f, 0x00, 0x00,
-                0x15, 0x38, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-                0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
-            ]
-        );
-    }
+		assert_eq!(
+			data,
+			vec![
+				0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+				0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+				0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x7b, 0x00, 0x00, 0x02, 0x1f, 0x00, 0x00,
+				0x15, 0x38, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+				0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+			]
+		);
+	}
 
-    #[test]
-    fn read_page() {
-        let storage = PageStorage::new(
-            vec![
-                0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-                0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-                0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-                0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-                0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x12, 0x34,
-                0x56, 0x78,
-            ],
-            4,
-        );
+	#[test]
+	fn read_page() {
+		let storage = PageStorage::new(
+			vec![
+				0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+				0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+				0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+				0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+				0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x12, 0x34,
+				0x56, 0x78,
+			],
+			4,
+		);
 
-        let mut buf: [u8; 4] = Default::default();
-        storage.read_page(&mut buf, 1).unwrap();
-        assert_eq!(buf, [0x12, 0x34, 0x56, 0x78]);
-    }
+		let mut buf: [u8; 4] = Default::default();
+		storage.read_page(&mut buf, 1).unwrap();
+		assert_eq!(buf, [0x12, 0x34, 0x56, 0x78]);
+	}
 
-    #[test]
-    fn write_page() {
-        let mut storage = PageStorage::new(Vec::new(), 4);
-        storage.write_page(&[0x23, 0x89, 0x43, 0x79], 3).unwrap();
+	#[test]
+	fn write_page() {
+		let mut storage = PageStorage::new(Vec::new(), 4);
+		storage.write_page(&[0x23, 0x89, 0x43, 0x79], 3).unwrap();
 
-        assert_eq!(
-            storage.file,
-            vec![
-                0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-                0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-                0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-                0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-                0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-                0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x23, 0x89, 0x43, 0x79,
-            ]
-        )
-    }
+		assert_eq!(
+			storage.file,
+			vec![
+				0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+				0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+				0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+				0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+				0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+				0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x23, 0x89, 0x43, 0x79,
+			]
+		)
+	}
 }
