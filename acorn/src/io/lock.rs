0817bdf17071dcab82e19587f93c1c2df1aeb70e
nicholas-roether <nicholas-roether@t-online.de>
2023-12-25 18:03:37 +0100

    Implement PageLocker

---
+++ acorn/src/io/lock.rs
@@ -0,0 +1,58 @@
+use std::{
+	iter,
+	sync::atomic::{AtomicU32, Ordering},
+	usize,
+};
+
+use parking_lot::{lock_api::RawRwLock as _, RawRwLock, RwLock};
+
+pub struct PageLocker {
+	locks_len: AtomicU32,
+	locks: RwLock<Vec<RawRwLock>>,
+}
+
+impl PageLocker {
+	#[inline]
+	pub fn new() -> Self {
+		Self {
+			locks_len: AtomicU32::new(0),
+			locks: RwLock::new(Vec::new()),
+		}
+	}
+
+	pub fn lock_shared(&self, page_num: u32) {
+		self.ensure_has_lock(page_num);
+		let locks = self.locks.read();
+		locks[page_num as usize].lock_shared();
+	}
+
+	pub unsafe fn unlock_shared(&self, page_num: u32) {
+		let locks = self.locks.read();
+		locks[page_num as usize].unlock_shared();
+	}
+
+	pub fn lock_exclusive(&self, page_num: u32) {
+		self.ensure_has_lock(page_num);
+		let locks = self.locks.read();
+		locks[page_num as usize].lock_exclusive();
+	}
+
+	pub unsafe fn unlock_exclusive(&self, page_num: u32) {
+		let locks = self.locks.read();
+		locks[page_num as usize].unlock_exclusive();
+	}
+
+	fn ensure_has_lock(&self, page_num: u32) {
+		if self.locks_len.load(Ordering::Acquire) <= page_num {
+			let new_length = page_num + 1;
+			self.extend_until(new_length as usize);
+			self.locks_len.store(new_length, Ordering::Release);
+		}
+	}
+
+	fn extend_until(&self, new_length: usize) {
+		let mut locks_mut = self.locks.write();
+		let extend_by = new_length - locks_mut.len();
+		locks_mut.extend(iter::repeat_with(|| RawRwLock::INIT).take(extend_by));
+	}
+}
