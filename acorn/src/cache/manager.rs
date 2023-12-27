76823011f16e5167dca929e80fac32e1b7b731fa
nicholas-roether <nicholas-roether@t-online.de>
2023-12-27 20:41:58 +0100

    Implement cache manager

---
+++ acorn/src/cache/manager.rs
@@ -0,0 +1,118 @@
+use std::{collections::VecDeque, num::NonZeroU32};
+
+#[derive(Debug)]
+pub struct CacheManager {
+	slow: VecDeque<NonZeroU32>,
+	fast_cap: usize,
+	fast: VecDeque<NonZeroU32>,
+	graveyard_cap: usize,
+	graveyard: VecDeque<NonZeroU32>,
+}
+
+impl CacheManager {
+	pub fn new(length: usize) -> Self {
+		debug_assert!(length >= 4);
+		Self {
+			slow: VecDeque::new(),
+			fast_cap: length / 4,
+			fast: VecDeque::new(),
+			graveyard_cap: length / 2,
+			graveyard: VecDeque::new(),
+		}
+	}
+
+	pub fn access(&mut self, item: NonZeroU32) -> bool {
+		if self.fast.contains(&item) {
+			return false;
+		}
+		if let Some(index) = self.slow.iter().position(|v| *v == item) {
+			self.slow.remove(index);
+			self.slow.push_front(item);
+			return false;
+		}
+		if self.graveyard.contains(&item) {
+			self.slow.push_front(item);
+			return true;
+		}
+		self.fast.push_front(item);
+		true
+	}
+
+	pub fn reclaim(&mut self) -> Option<NonZeroU32> {
+		if self.fast.len() > self.fast_cap {
+			let reclaimed = self.fast.pop_back().unwrap();
+			self.graveyard.push_front(reclaimed);
+			if self.graveyard.len() > self.graveyard_cap {
+				self.graveyard.pop_back();
+			}
+			return Some(reclaimed);
+		}
+
+		self.slow.pop_back()
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	#[test]
+	fn fast_fifo() {
+		let mut mgr = CacheManager::new(8);
+
+		// Flood the fast queue
+		assert!(mgr.access(NonZeroU32::new(1).unwrap()));
+		assert!(mgr.access(NonZeroU32::new(2).unwrap()));
+		assert!(mgr.access(NonZeroU32::new(3).unwrap()));
+		assert!(mgr.access(NonZeroU32::new(4).unwrap()));
+		assert!(mgr.access(NonZeroU32::new(5).unwrap()));
+
+		// Shouldn't try to re-store stuff that's already in the queue
+		// (Also, order of subsequent accesses should not matter)
+		assert!(!mgr.access(NonZeroU32::new(2).unwrap()));
+		assert!(!mgr.access(NonZeroU32::new(1).unwrap()));
+		assert!(!mgr.access(NonZeroU32::new(3).unwrap()));
+
+		// Should immediately reclaim the tail of the fast queue
+		assert_eq!(mgr.reclaim(), Some(NonZeroU32::new(1).unwrap()));
+		assert_eq!(mgr.reclaim(), Some(NonZeroU32::new(2).unwrap()));
+		assert_eq!(mgr.reclaim(), Some(NonZeroU32::new(3).unwrap()));
+		assert_eq!(mgr.reclaim(), None);
+
+		// Should need to re-store the evicted values
+		assert!(mgr.access(NonZeroU32::new(1).unwrap()));
+		assert!(mgr.access(NonZeroU32::new(2).unwrap()));
+		assert!(mgr.access(NonZeroU32::new(3).unwrap()));
+	}
+
+	#[test]
+	fn slow_lru_and_graveyard() {
+		let mut mgr = CacheManager::new(8);
+
+		// Flood the fast queue
+		mgr.access(NonZeroU32::new(1).unwrap());
+		mgr.access(NonZeroU32::new(2).unwrap());
+		mgr.access(NonZeroU32::new(3).unwrap());
+		mgr.access(NonZeroU32::new(69).unwrap());
+		mgr.access(NonZeroU32::new(420).unwrap());
+
+		// Reclaim to make shure 1, 2, and 3 are in the graveyard
+		mgr.reclaim();
+		mgr.reclaim();
+		mgr.reclaim();
+
+		// Resurrect 1, 2, and 3 from the graveyard to the slow queue
+		mgr.access(NonZeroU32::new(1).unwrap());
+		mgr.access(NonZeroU32::new(2).unwrap());
+		mgr.access(NonZeroU32::new(3).unwrap());
+
+		// This should influence the order of reclaiming
+		mgr.access(NonZeroU32::new(1).unwrap());
+		mgr.access(NonZeroU32::new(3).unwrap());
+
+		// Should reclaim from slow queue according to LRU
+		assert_eq!(mgr.reclaim(), Some(NonZeroU32::new(2).unwrap()));
+		assert_eq!(mgr.reclaim(), Some(NonZeroU32::new(1).unwrap()));
+		assert_eq!(mgr.reclaim(), Some(NonZeroU32::new(3).unwrap()));
+	}
+}
