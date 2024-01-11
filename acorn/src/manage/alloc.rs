53892d447fb9aa90086ccdbddeb00e9a3d20eaa3
Nicholas Roether <nicholas.roether@t-online.de>
2024-01-11 08:07:02 +0100

    Eventually i'll actually finish this thing

--- acorn/src/manage/alloc.rs
+++ acorn/src/manage/alloc.rs
@@ -1,7 +1,25 @@
 use parking_lot::RwLock;
 
+use crate::utils::array_map::ArrayMap;
+
 use super::segment_alloc::SegmentAllocManager;
 
 pub struct AllocManager {
-	segments: RwLock<Vec<SegmentAllocManager>>,
+	state: RwLock<State>,
+}
+
+impl AllocManager {
+	pub fn new() -> Self {
+		Self {
+			state: RwLock::new(State {
+				segments: ArrayMap::new(),
+				free_stack: Vec::new(),
+			}),
+		}
+	}
+}
+
+struct State {
+	segments: ArrayMap<SegmentAllocManager>,
+	free_stack: Vec<u32>,
 }
