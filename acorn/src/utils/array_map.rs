f142ca522c4711b87944cb6a3b5b003440a08c83
Nicholas Roether <nicholas.roether@t-online.de>
2024-01-11 08:27:13 +0100

    Fix array_map AGAIN and add tests this time

--- acorn/src/utils/array_map.rs
+++ acorn/src/utils/array_map.rs
@@ -1,4 +1,4 @@
-use std::iter;
+use std::{iter, slice};
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct ArrayMap<T> {
@@ -25,6 +25,10 @@ impl<T> ArrayMap<T> {
 		self.slots.get(key)?.as_ref()
 	}
 
+	pub fn delete(&mut self, key: usize) -> Option<T> {
+		self.slots.get_mut(key)?.take()
+	}
+
 	pub fn iter(&self) -> Iter<T> {
 		Iter::new(&self.slots)
 	}
@@ -39,7 +43,7 @@ impl<T> ArrayMap<T> {
 
 	fn make_space_for(&mut self, key: usize) {
 		if key >= self.slots.len() {
-			let extend_by = self.slots.len() - key + 1;
+			let extend_by = key - self.slots.len() + 1;
 			self.slots
 				.extend(iter::repeat_with(|| None).take(extend_by))
 		}
@@ -54,12 +58,15 @@ impl<T> Default for ArrayMap<T> {
 
 pub struct Iter<'a, T> {
 	key: usize,
-	slots: &'a [Option<T>],
+	slots: slice::Iter<'a, Option<T>>,
 }
 
 impl<'a, T> Iter<'a, T> {
 	fn new(slots: &'a [Option<T>]) -> Self {
-		Self { key: 0, slots }
+		Self {
+			key: 0,
+			slots: slots.iter(),
+		}
 	}
 }
 
@@ -67,27 +74,28 @@ impl<'a, T> Iterator for Iter<'a, T> {
 	type Item = (usize, &'a T);
 
 	fn next(&mut self) -> Option<Self::Item> {
-		let item = loop {
-			if self.key == self.slots.len() {
-				return None;
-			}
-			if let Some(item) = self.slots[self.key].as_ref() {
-				break item;
-			}
+		for slot in self.slots.by_ref() {
+			let key = self.key;
 			self.key += 1;
-		};
-		Some((self.key, item))
+			if let Some(item) = slot {
+				return Some((key, item));
+			}
+		}
+		None
 	}
 }
 
 pub struct IterMut<'a, T> {
 	key: usize,
-	slots: &'a mut [Option<T>],
+	slots: slice::IterMut<'a, Option<T>>,
 }
 
 impl<'a, T> IterMut<'a, T> {
 	fn new(slots: &'a mut [Option<T>]) -> Self {
-		Self { key: 0, slots }
+		Self {
+			key: 0,
+			slots: slots.iter_mut(),
+		}
 	}
 }
 
@@ -95,16 +103,14 @@ impl<'a, T> Iterator for IterMut<'a, T> {
 	type Item = (usize, &'a mut T);
 
 	fn next(&mut self) -> Option<Self::Item> {
-		let item = loop {
-			if self.key == self.slots.len() {
-				return None;
-			}
-			if let Some(item) = self.slots[self.key].as_mut() {
-				break item;
-			}
+		for slot in self.slots.by_ref() {
+			let key = self.key;
 			self.key += 1;
-		};
-		Some((self.key, item))
+			if let Some(item) = slot {
+				return Some((key, item));
+			}
+		}
+		None
 	}
 }
 
@@ -123,5 +129,74 @@ impl<'a, T> IntoIterator for &'a mut ArrayMap<T> {
 
 	fn into_iter(self) -> Self::IntoIter {
 		self.iter_mut()
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	#[test]
+	fn simple_insert_get() {
+		let mut map: ArrayMap<&'static str> = ArrayMap::new();
+		map.insert(69, "Some value");
+		map.insert(420, "Some other value");
+
+		assert_eq!(map.get(69), Some(&"Some value"));
+		assert_eq!(map.get(420), Some(&"Some other value"));
+	}
+
+	#[test]
+	fn try_get_non_existent_key() {
+		let map: ArrayMap<u32> = ArrayMap::new();
+
+		assert_eq!(map.get(2), None);
+		assert_eq!(map.get(0), None);
+	}
+
+	#[test]
+	fn overwrite_values() {
+		let mut map: ArrayMap<&'static str> = ArrayMap::new();
+		map.insert(25, "AAAAAAAAAAAAA");
+		map.insert(25, "BBBBBBBB");
+
+		assert_eq!(map.get(25), Some(&"BBBBBBBB"));
+	}
+
+	#[test]
+	fn delete_values() {
+		let mut map: ArrayMap<&'static str> = ArrayMap::new();
+		map.insert(25, "That's me :)");
+		map.delete(25);
+
+		assert_eq!(map.get(25), None);
+	}
+
+	#[test]
+	fn iter_items() {
+		let mut map: ArrayMap<u32> = ArrayMap::new();
+		map.insert(25, 69);
+		map.insert(69, 420);
+		map.insert(420, 25);
+
+		let mut iter = map.iter();
+		assert_eq!(iter.next(), Some((25, &69)));
+		assert_eq!(iter.next(), Some((69, &420)));
+		assert_eq!(iter.next(), Some((420, &25)));
+		assert_eq!(iter.next(), None);
+	}
+
+	#[test]
+	fn iter_items_mut() {
+		let mut map: ArrayMap<u32> = ArrayMap::new();
+		map.insert(25, 69);
+		map.insert(69, 420);
+		map.insert(420, 25);
+
+		let mut iter = map.iter_mut();
+		assert_eq!(iter.next(), Some((25, &mut 69)));
+		assert_eq!(iter.next(), Some((69, &mut 420)));
+		assert_eq!(iter.next(), Some((420, &mut 25)));
+		assert_eq!(iter.next(), None);
 	}
 }
