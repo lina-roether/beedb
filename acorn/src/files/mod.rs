331b149c3238fd450477dbd5aed7490e7400d452
Nicholas Roether <nicholas.roether@t-online.de>
2024-04-12 14:38:29 +0200

    Migrate to musli_zerocopy

--- acorn/src/files/mod.rs
+++ acorn/src/files/mod.rs
@@ -1,40 +1,9 @@
-use std::{error::Error, io};
+use std::{io, mem::size_of};
 
-use num_enum::{IntoPrimitive, TryFromPrimitive};
+use musli_zerocopy::ZeroCopy;
 use thiserror::Error;
-use zerocopy::{AsBytes, FromBytes, FromZeroes};
 
 pub(crate) mod wal;
-
-#[derive(Debug, AsBytes, FromZeroes, FromBytes)]
-#[repr(C, align(2))]
-pub(super) struct GenericHeader {
-	magic: [u8; 4],
-	byte_order: u8,
-	file_type: u8,
-	content_offset: u16,
-}
-
-const MAGIC: [u8; 4] = *b"ACRN";
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq, AsBytes, IntoPrimitive, TryFromPrimitive)]
-#[repr(u8)]
-pub(super) enum FileType {
-	Wal = 0,
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq, AsBytes, IntoPrimitive, TryFromPrimitive)]
-#[repr(u8)]
-enum ByteOrder {
-	BigEndian = 0,
-	LittleEndian = 1,
-}
-
-#[cfg(target_endian = "big")]
-const NATIVE_BYTE_ORDER: ByteOrder = ByteOrder::BigEndian;
-
-#[cfg(target_endian = "little")]
-const NATIVE_BYTE_ORDER: ByteOrder = ByteOrder::LittleEndian;
 
 #[derive(Debug, Error)]
 pub(crate) enum FileError {
@@ -45,7 +14,7 @@ pub(crate) enum FileError {
 	ByteOrderMismatch,
 
 	#[error("The file is corrupted: {0}")]
-	Corrupted(#[source] Box<dyn Error>),
+	Corrupted(#[from] musli_zerocopy::Error),
 
 	#[error("Unexpected file type {0:?}")]
 	WrongFileType(FileType),
@@ -54,45 +23,65 @@ pub(crate) enum FileError {
 	Io(#[from] io::Error),
 }
 
-impl FileError {
-	fn corrupted(source: impl Error + 'static) -> Self {
-		Self::Corrupted(Box::new(source))
-	}
+#[derive(Debug, Clone, Copy, PartialEq, Eq, ZeroCopy)]
+#[repr(u8)]
+pub(super) enum FileType {
+	Wal = 0,
 }
 
-#[derive(Debug)]
-pub(super) struct GenericMeta {
-	pub file_type: FileType,
-	pub content_offset: u16,
+#[derive(Debug, Clone, Copy, PartialEq, Eq, ZeroCopy)]
+#[repr(u8)]
+enum ByteOrder {
+	BigEndian = 0,
+	LittleEndian = 1,
 }
 
-impl From<&GenericMeta> for GenericHeader {
-	fn from(value: &GenericMeta) -> Self {
+#[derive(Debug, ZeroCopy)]
+#[repr(C)]
+pub(super) struct GenericHeader {
+	magic: [u8; 4],
+	byte_order: ByteOrder,
+	file_type: FileType,
+	content_offset: u16,
+}
+
+const MAGIC: [u8; 4] = *b"ACRN";
+
+#[cfg(target_endian = "big")]
+const NATIVE_BYTE_ORDER: ByteOrder = ByteOrder::BigEndian;
+
+#[cfg(target_endian = "little")]
+const NATIVE_BYTE_ORDER: ByteOrder = ByteOrder::LittleEndian;
+
+#[derive(Debug, Clone)]
+pub(super) struct GenericHeaderInit {
+	file_type: FileType,
+	header_size: u16,
+}
+
+impl GenericHeader {
+	pub fn new(
+		GenericHeaderInit {
+			file_type,
+			header_size,
+		}: GenericHeaderInit,
+	) -> Self {
 		Self {
 			magic: MAGIC,
-			byte_order: NATIVE_BYTE_ORDER.into(),
-			file_type: value.file_type.into(),
-			content_offset: value.content_offset,
+			byte_order: NATIVE_BYTE_ORDER,
+			file_type,
+			content_offset: (size_of::<Self>() as u16) + header_size,
 		}
 	}
-}
 
-impl TryFrom<&GenericHeader> for GenericMeta {
-	type Error = FileError;
-
-	fn try_from(value: &GenericHeader) -> Result<Self, Self::Error> {
-		if value.magic != MAGIC {
+	pub fn validate(&self) -> Result<(), FileError> {
+		if self.magic != MAGIC {
 			return Err(FileError::MissingMagic);
 		}
-		let byte_order = ByteOrder::try_from(value.byte_order).map_err(FileError::corrupted)?;
-		if byte_order != NATIVE_BYTE_ORDER {
+		if self.byte_order != NATIVE_BYTE_ORDER {
 			return Err(FileError::ByteOrderMismatch);
 		}
-		let file_type = FileType::try_from(value.file_type).map_err(FileError::corrupted)?;
-		Ok(Self {
-			file_type,
-			content_offset: value.content_offset,
-		})
+		return Ok(());
 	}
 }
 
@@ -101,32 +90,30 @@ mod tests {
 	use super::*;
 
 	#[test]
-	fn meta_from_header() {
+	fn verify_header() {
 		let header = GenericHeader {
 			magic: *b"ACRN",
 			byte_order: NATIVE_BYTE_ORDER.into(),
 			file_type: FileType::Wal.into(),
 			content_offset: 69,
 		};
-		let meta = GenericMeta::try_from(&header).unwrap();
-		assert_eq!(meta.file_type, FileType::Wal);
-		assert_eq!(meta.content_offset, 69);
+		assert!(header.validate().is_ok());
 	}
 
 	#[test]
-	fn try_meta_from_header_missing_magic() {
+	fn try_verify_header_with_missing_magic() {
 		let header = GenericHeader {
 			magic: *b"KEKW",
 			byte_order: NATIVE_BYTE_ORDER.into(),
 			file_type: FileType::Wal.into(),
 			content_offset: 69,
 		};
-		let err = GenericMeta::try_from(&header).unwrap_err();
+		let err = header.validate().unwrap_err();
 		assert_eq!(err.to_string(), "The file is not an acorn database file");
 	}
 
 	#[test]
-	fn try_meta_from_header_byte_order_mismatch() {
+	fn try_verify_header_with_byte_order_mismatch() {
 		let header = GenericHeader {
 			magic: *b"ACRN",
 			byte_order: match NATIVE_BYTE_ORDER {
@@ -136,40 +123,10 @@ mod tests {
 			file_type: FileType::Wal.into(),
 			content_offset: 69,
 		};
-		let err = GenericMeta::try_from(&header).unwrap_err();
+		let err = header.validate().unwrap_err();
 		assert_eq!(
 			err.to_string(),
 			"The file was created on a platform with a different byte order and cannot be opened"
-		);
-	}
-
-	#[test]
-	fn try_meta_from_header_corrupted_byte_order() {
-		let header = GenericHeader {
-			magic: *b"ACRN",
-			byte_order: 123,
-			file_type: FileType::Wal.into(),
-			content_offset: 69,
-		};
-		let err = GenericMeta::try_from(&header).unwrap_err();
-		assert_eq!(
-			err.to_string(),
-			"The file is corrupted: No discriminant in enum `ByteOrder` matches the value `123`"
-		);
-	}
-
-	#[test]
-	fn try_meta_from_header_corrupted_file_type() {
-		let header = GenericHeader {
-			magic: *b"ACRN",
-			byte_order: NATIVE_BYTE_ORDER.into(),
-			file_type: 123,
-			content_offset: 69,
-		};
-		let err = GenericMeta::try_from(&header).unwrap_err();
-		assert_eq!(
-			err.to_string(),
-			"The file is corrupted: No discriminant in enum `FileType` matches the value `123`"
 		);
 	}
 }
--- acorn/src/files/wal.rs
+++ acorn/src/files/wal.rs
@@ -1,19 +1,24 @@
 use std::{
-	borrow::Borrow,
 	io::{Read, Seek, SeekFrom, Write},
 	mem::size_of,
 };
 
-use aligned::{Aligned, A2};
-use num_enum::{IntoPrimitive, TryFromPrimitive};
-use zerocopy::{AsBytes, FromBytes, FromZeroes};
+use musli_zerocopy::{OwnedBuf, Ref, ZeroCopy};
 
-use super::{FileError, FileType, GenericHeader, GenericMeta};
+use super::{FileError, FileType, GenericHeader, GenericHeaderInit};
 
-#[derive(Debug, AsBytes, FromZeroes, FromBytes)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, ZeroCopy)]
+#[repr(u8)]
+enum ItemType {
+	Write = 0,
+	Commit = 1,
+	Undo = 2,
+}
+
+#[derive(Debug, ZeroCopy)]
 #[repr(C)]
 struct ItemHeader {
-	item_type: u8,
+	item_type: ItemType,
 	flags: u8,
 	data_length: u16,
 	crc: u32,
@@ -21,55 +26,10 @@ struct ItemHeader {
 	sequence_num: u64,
 }
 
-#[derive(Debug, AsBytes, FromZeroes, FromBytes)]
+#[derive(Debug, ZeroCopy)]
 #[repr(C)]
 struct ItemFooter {
 	start_offset: i16,
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq, AsBytes, IntoPrimitive, TryFromPrimitive)]
-#[repr(u8)]
-pub(super) enum ItemType {
-	Write = 0,
-	Commit = 1,
-	Undo = 2,
-}
-
-#[derive(Debug)]
-pub(super) struct ItemMeta {
-	pub item_type: ItemType,
-	pub data_length: u16,
-	pub crc: u32,
-	pub transaction_id: u64,
-	pub sequence_num: u64,
-}
-
-impl From<&ItemMeta> for ItemHeader {
-	fn from(value: &ItemMeta) -> Self {
-		Self {
-			item_type: value.item_type.into(),
-			flags: 0,
-			data_length: value.data_length,
-			crc: value.crc,
-			transaction_id: value.transaction_id,
-			sequence_num: value.sequence_num,
-		}
-	}
-}
-
-impl TryFrom<&ItemHeader> for ItemMeta {
-	type Error = FileError;
-
-	fn try_from(value: &ItemHeader) -> Result<Self, Self::Error> {
-		let item_type = ItemType::try_from(value.item_type).map_err(FileError::corrupted)?;
-		Ok(Self {
-			item_type,
-			data_length: value.data_length,
-			crc: value.crc,
-			transaction_id: value.transaction_id,
-			sequence_num: value.sequence_num,
-		})
-	}
 }
 
 pub(crate) struct WalFile<F: Seek + Read + Write> {
@@ -78,32 +38,34 @@ pub(crate) struct WalFile<F: Seek + Read + Write> {
 }
 
 impl<F: Seek + Read + Write> WalFile<F> {
-	const BODY_START: u16 = size_of::<GenericHeader>() as u16;
-
 	fn create(mut file: F) -> Result<Self, FileError> {
 		file.seek(SeekFrom::Start(0))?;
-		let meta = GenericMeta {
+		let mut meta = GenericHeader::new(GenericHeaderInit {
 			file_type: FileType::Wal,
-			content_offset: Self::BODY_START,
-		};
-		file.write_all(GenericHeader::from(&meta).as_bytes())?;
+			header_size: 0,
+		});
+		file.write_all(meta.to_bytes())?;
 
 		Ok(Self {
 			file,
-			body_start: Self::BODY_START.into(),
+			body_start: meta.content_offset.into(),
 		})
 	}
 
 	fn open(mut file: F) -> Result<Self, FileError> {
 		file.seek(SeekFrom::Start(0))?;
-		let mut header_bytes: Aligned<A2, [u8; size_of::<GenericHeader>()]> = Default::default();
-		file.read_exact(header_bytes.as_mut_slice())?;
-		let meta = GenericMeta::try_from(GenericHeader::ref_from(header_bytes.borrow()).unwrap())?;
-		if meta.file_type != FileType::Wal {
-			return Err(FileError::WrongFileType(meta.file_type));
+		let mut header_buf =
+			OwnedBuf::with_capacity_and_alignment::<GenericHeader>(size_of::<GenericHeader>());
+		file.read_exact(header_buf.as_mut_slice())?;
+
+		let header: &GenericHeader = header_buf.load_at(0)?;
+
+		header.validate()?;
+		if header.file_type != FileType::Wal {
+			return Err(FileError::WrongFileType(header.file_type));
 		}
 		Ok(Self {
-			body_start: meta.content_offset.into(),
+			body_start: header.content_offset.into(),
 			file,
 		})
 	}
@@ -111,56 +73,19 @@ impl<F: Seek + Read + Write> WalFile<F> {
 
 #[cfg(test)]
 mod tests {
-	use std::{borrow::Borrow, io::Cursor};
+	use std::io::Cursor;
 
 	use super::*;
-
-	#[test]
-	fn item_meta_from_header() {
-		let item_header = ItemHeader {
-			item_type: ItemType::Write.into(),
-			flags: 0,
-			data_length: 69,
-			crc: 420,
-			transaction_id: 25,
-			sequence_num: 24,
-		};
-		let meta = ItemMeta::try_from(&item_header).unwrap();
-		assert_eq!(meta.item_type, ItemType::Write);
-		assert_eq!(meta.data_length, 69);
-		assert_eq!(meta.crc, 420);
-		assert_eq!(meta.transaction_id, 25);
-		assert_eq!(meta.sequence_num, 24);
-	}
-
-	#[test]
-	fn try_item_meta_from_header_corrupted_item_type() {
-		let item_header = ItemHeader {
-			item_type: 123,
-			flags: 0,
-			data_length: 69,
-			crc: 420,
-			transaction_id: 25,
-			sequence_num: 24,
-		};
-		let err = ItemMeta::try_from(&item_header).unwrap_err();
-		assert_eq!(
-			err.to_string(),
-			"The file is corrupted: No discriminant in enum `ItemType` matches the value `123`"
-		);
-	}
 
 	#[test]
 	fn create_wal_file() {
 		let mut file: Cursor<Vec<u8>> = Cursor::new(Vec::new());
 		WalFile::create(&mut file).unwrap();
 
-		let mut aligned: Aligned<A2, [u8; size_of::<GenericHeader>()]> =
-			Aligned(Default::default());
-		aligned.copy_from_slice(&file.into_inner());
-		let meta =
-			GenericMeta::try_from(GenericHeader::ref_from(aligned.borrow()).unwrap()).unwrap();
-		assert_eq!(meta.file_type, FileType::Wal);
-		assert_eq!(meta.content_offset, size_of::<GenericHeader>() as u16);
+		let mut buf = OwnedBuf::new();
+		buf.extend_from_slice(&file.into_inner());
+		let header: &GenericHeader = buf.load_at(0).unwrap();
+		assert_eq!(header.file_type, FileType::Wal);
+		assert_eq!(header.content_offset, size_of::<GenericHeader>() as u16);
 	}
 }
