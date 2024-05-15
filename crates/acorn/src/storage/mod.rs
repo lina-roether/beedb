079af4e07e975eea71cf206612fb542ca3638e31
niko-roether <nicholas.roether@t-online.de>
2024-05-15 12:57:54 +0200

    Implement reading from PageStorage

--- crates/acorn/src/storage/mod.rs
+++ crates/acorn/src/storage/mod.rs
@@ -1,3 +1,5 @@
+use std::ops::Deref;
+use std::ops::DerefMut;
 use std::sync::Arc;
 
 use thiserror::Error;
@@ -9,17 +11,18 @@ use crate::files::segment::PAGE_BODY_SIZE;
 use crate::files::DatabaseFolder;
 use crate::files::FileError;
 
-mod cache;
-mod physical;
-mod wal;
-
 pub(crate) use crate::files::PageId;
 use crate::files::TransactionState;
 use crate::files::WalIndex;
 
 use cache::{PageCache, PageCacheApi, PageCacheConfig};
 use physical::{PhysicalStorage, PhysicalStorageApi, PhysicalStorageConfig};
+
 use wal::{Wal, WalApi, WalConfig};
+
+mod cache;
+mod physical;
+mod wal;
 
 #[derive(Debug, Error)]
 pub(crate) enum StorageError {
@@ -86,11 +89,32 @@ where
 			wal,
 		}
 	}
+
+	fn load_into_cache(&self, page_id: PageId) -> Result<PC::WriteGuard<'_>, StorageError> {
+		let mut guard = self.cache.store(page_id);
+		if let Err(error) = self.physical.read(page_id, &mut guard) {
+			self.cache.scrap(page_id);
+			return Err(error);
+		}
+		Ok(guard)
+	}
 }
 
-#[cfg_attr(test, automock)]
+#[cfg_attr(test, automock(
+    type ReadGuard<'a> = &'a [u8];
+    type WriteGuard<'a> = &'a mut [u8];
+))]
+#[allow(clippy::needless_lifetimes)]
 pub(crate) trait PageStorageApi {
+	type ReadGuard<'a>: Deref<Target = [u8]> + 'a
+	where
+		Self: 'a;
+	type WriteGuard<'a>: Deref<Target = [u8]> + DerefMut + 'a
+	where
+		Self: 'a;
+
 	fn recover(&self) -> Result<(), StorageError>;
+	fn read<'a>(&'a self, page_id: PageId) -> Result<Self::ReadGuard<'a>, StorageError>;
 }
 
 impl<PS, PC, W> PageStorageApi for PageStorage<PS, PC, W>
@@ -99,6 +123,9 @@ where
 	PC: PageCacheApi,
 	W: WalApi,
 {
+	type ReadGuard<'a> = PC::ReadGuard<'a> where Self: 'a;
+	type WriteGuard<'a> = PC::WriteGuard<'a> where Self: 'a;
+
 	fn recover(&self) -> Result<(), StorageError> {
 		self.wal.recover(|write_op| {
 			// TODO: This can probably be abstracted to use the page cache somehow
@@ -110,6 +137,11 @@ where
 				.write(write_op.page_id, write_op.buf, write_op.index)?;
 			Ok(())
 		})
+	}
+
+	fn read(&self, page_id: PageId) -> Result<Self::ReadGuard<'_>, StorageError> {
+		let guard = self.load_into_cache(page_id)?;
+		Ok(self.cache.downgrade_guard(guard))
 	}
 }
 
